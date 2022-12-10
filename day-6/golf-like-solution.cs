91c56c3a1cbaf87b652e291fd3c993cbc492f447
assembelly <alfred.fyro@protonmail.com>
2022-12-10 02:35:43 +0100

    added golf-like solution to day 6

---
+++ day-6/golf-like-solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day_6_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datastream = File.ReadAllText(@"..\..\..\..\input");
+            int index = 0;
+            int sequenceLength = 14;
+
+            for (; new HashSet<char>(datastream.Substring(index, sequenceLength)).Count < sequenceLength; index++) ;            
+
+            Console.WriteLine(index + sequenceLength);
+        }
+    }
+}
