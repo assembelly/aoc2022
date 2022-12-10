8fe68b5ad7e41e854186bac6cbe6a049e170f227
assembelly <alfred.fyro@protonmail.com>
2022-12-10 01:27:16 +0100

    added day 6

---
+++ day-6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day_6_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datastream = File.ReadAllText(@"..\..\..\..\input");
+            int sequenceLengt = 14;
+                        
+            string startingWindow = datastream.Substring(0, sequenceLengt);
+            Queue<char> slidingWindow = new Queue<char>(startingWindow);   
+                                 
+            int uniqueCharCount = new HashSet<char>(slidingWindow).Count;
+
+            int index = sequenceLengt;
+
+            while (uniqueCharCount < sequenceLengt)
+            {
+                slidingWindow.Dequeue();
+                slidingWindow.Enqueue(datastream[index]);
+
+                uniqueCharCount = new HashSet<char>(slidingWindow).Count;
+
+                index++;
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
